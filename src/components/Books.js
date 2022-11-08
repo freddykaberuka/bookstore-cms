5b636fc37cf1f71d8d1c9e0d89048b136d72663c
freddykaberuka <freddykaberuka2009@gmail.com>
2022-11-08 14:01:09 +0200

    add reducer and action

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AddBook, RemoveBook } from '../redux/books/books';
 import Book from './Book';
 import BookForm from './BookForm';
 
-function Books() {
+const Books = () => {
+  const dispatch = useDispatch();
+  const StoredBook = useSelector((state) => state.BooksReducer);
+
+  const AddNewBook = (e, NewBook) => {
+    e.preventDefault();
+    dispatch(AddBook(NewBook));
+  };
+
+  const RemoveExistingBook = (id) => {
+    dispatch(RemoveBook(id));
+  };
   return (
     <div>
-      <Book title="Book Title" author="Book Author" />
-      <BookForm />
+      <>
+        {StoredBook.map((item) => (
+          <Book
+            title={item.title}
+            author={item.author}
+            key={item.id}
+            id={item.id}
+            RemoveExistingBook={RemoveExistingBook}
+          />
+        ))}
+        <BookForm AddNewBook={AddNewBook} />
+      </>
     </div>
   );
-}
+};
 
 export default Books;
---
+++ src/redux/books/books.js
@@ -0,0 +1,35 @@
+const ADD_BOOK = 'bookstore-cms/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore-cms/books/REMOVE_BOOK';
+const books = [];
+
+// action
+export const AddBook = (NewBook) => ({
+  type: ADD_BOOK,
+  payload: NewBook,
+});
+
+export const RemoveBook = (id) => ({
+  type: REMOVE_BOOK,
+  id,
+});
+
+// reducer
+
+const BooksReducer = (state = books, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          title: action.payload.title,
+          author: action.payload.author,
+        },
+      ];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state;
+  }
+};
+export default BooksReducer;
